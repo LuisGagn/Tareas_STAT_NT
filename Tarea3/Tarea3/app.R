527791db99f83fc4224967c5fc6ae480388fc5f8
LuisGagn <luisgagnevin@gmail.com>
2021-07-01 23:27:33 -0300

    Tarea 3 | Terminar Extra | Publish

--- Tarea3/Tarea3/app.R
+++ Tarea3/Tarea3/app.R
@@ -7,9 +7,8 @@ propinas <- read_csv("propina.csv" )
 
 ui <- fluidPage(
     titlePanel("Datos de Propina"),
-    
     sidebarLayout(
-        sidebarPanel(
+    sidebarPanel(
             actionButton("Prende", "Generar Graficos",width = 225),
             actionButton("Apaga", "Borrar Graficos", width = 225),
             selectInput('varcolor', 'Variable en color',
@@ -18,37 +17,29 @@ ui <- fluidPage(
         
         
         mainPanel(
-            tabsetPanel(
-                
+        tabsetPanel(
                 tabPanel("Bivariado",plotOutput("scat" ), dataTableOutput("tablita")),
                 tabPanel("Univariado", plotOutput("graf"))
-                
             ))
-    ))
-
-
-
-
-
-
-
-
+            ))
+    
 
 
 server <- function(input, output){
     
+   # BOTONES 
     v <- reactiveValues(data = NULL)
     
     observeEvent(input$Prende, {
         f= propinas
         v$d1 <- f
     })
+    
     observeEvent(input$Apaga, {
         v$d1 <- NULL
     })  
     
-    # TABLA    
-    
+  # TABLA    
     tabla<- reactive(
         if(is.null(v$d1)){return()}else{
         v$d1 %>% group_by(.data[[input$varcolor]])%>%
@@ -56,52 +47,35 @@ server <- function(input, output){
                           "mean_total"=mean(total), "sd_total"=sd(total))%>%
                 mutate(across(where(is.numeric), round, as.numeric(input$digitos)))})
 
+  # GRAFICOS
     
-    output$tablita <- 
-        
-        DT::renderDataTable(tabla())
-    
-    ################################################################################
-    
-    
-    # GRAFICO  
-  m<-reactive(      if(is.null(v$d1)){return()}else{    ggplot(data = v$d1,
-                             aes(x = total, y = propina,
-                                 colour = .data[[input$varcolor]] ))+
+  # GRAFICO 1   
+  G1<-reactive(if(is.null(v$d1)){return()}else{    
+                          ggplot(data = v$d1,
+                          aes(x = total, y = propina,
+                          colour = .data[[input$varcolor]] ))+
                           geom_point() + theme(aspect.ratio = 1) +
                           scale_x_continuous(name ="Total de la cuenta") +
                           scale_y_continuous(name = "Propina")}) 
     
-     output$scat <- 
+  # GRAFICO 2
+  G2<-reactive(if(is.null(v$d1)){return()}else{
+              v$d1 %>% group_by(.data[[input$varcolor]])%>% 
+              summarise(n=n())%>%
+              ggplot(aes(x=eval(as.name(input$varcolor)), y=n))+
+              geom_col()+
+              xlab(as.name(input$varcolor))+
+              ylab("Cantidad")})
+  
+  
     
+  # CALLS 
+        output$scat <- renderPlot(G1())
     
-        renderPlot(m()
-
-    )
+        output$graf <- renderPlot(G2())
     
-   k<-reactive(
-       if(is.null(v$d1)){return()}else{
-           v$d1 %>% group_by(.data[[input$varcolor]])%>% summarise(n=n())%>%
-        ggplot(aes(x=eval(as.name(input$varcolor)), y=n))+
-        geom_col()+
-        xlab(as.name(input$varcolor))+
-        ylab("Cantidad")})
-    
-    
-    
-    output$graf <- 
-        
-        renderPlot(k())
-    
-    
+        output$tablita <- DT::renderDataTable(tabla())
 }
-
-# GRAFICO 2
-
-# Datos
-
-
-
 
 
 
